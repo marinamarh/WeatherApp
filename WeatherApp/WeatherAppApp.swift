ee087784789b735dd2d6ccc1d344018d99922518
Marina Marhitych <margmarina9@gmail.com>
2026-05-29 17:53:49 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Marina Marhitych on 29.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
