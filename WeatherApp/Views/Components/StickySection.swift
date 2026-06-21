74e3fbb8a297b750a9dd95f7253fdff313c865c7
Marina Marhitych <margmarina9@gmail.com>
2026-06-21 21:00:04 +0300

    UI Update

---
+++ WeatherApp/Views/Components/StickySection.swift
@@ -0,0 +1,18 @@
+//
+//  StickySection.swift
+//  WeatherApp
+//
+//  Created by Marina Marhitych on 21.06.2026.
+//
+
+import SwiftUI
+
+struct StickySection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StickySection()
+}
