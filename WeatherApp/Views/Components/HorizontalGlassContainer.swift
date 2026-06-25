d22a060e2a7bcda3eb689c2ccca14769e9238e6e
Marina Marhitych <margmarina9@gmail.com>
2026-06-25 16:20:20 +0300

    UI update

---
+++ WeatherApp/Views/Components/HorizontalGlassContainer.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalGlassContainer.swift
+//  WeatherApp
+//
+//  Created by Marina Marhitych on 24.06.2026.
+//
+
+import SwiftUI
+
+struct HorizontalGlassContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalGlassContainer()
+}
